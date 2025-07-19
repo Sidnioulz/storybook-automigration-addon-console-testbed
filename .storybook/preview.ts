3f6edd48bd38af08bbc1132b7107b112f3656e13
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-07-19 15:09:16 +0200

    setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,18 +1,12 @@
-import assert from 'assert';
-
 import '@storybook/addon-console';
 
-export const parameters = {
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/i,
+export default {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
     },
   },
-}
-
-export const beforeEach = () => {
-  // Existing code.
-  const frog = 'frog';
-  assert(frog === 'frog', 'Frog should be frog');
 };
