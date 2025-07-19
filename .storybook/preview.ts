cc7386f495c208f8ade23a2a30c513e56b838baf
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-07-19 15:09:16 +0200

    setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,18 +1,25 @@
 import assert from 'assert';
+import type { Preview } from '@storybook/vue3'
 
 import '@storybook/addon-console';
 
-export const parameters = {
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/i,
-    },
-  },
-}
-
-export const beforeEach = () => {
+function beforeEach() {
   // Existing code.
   const frog = 'frog';
   assert(frog === 'frog', 'Frog should be frog');
+}
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+  beforeEach,
 };
+
+
+export default preview;
